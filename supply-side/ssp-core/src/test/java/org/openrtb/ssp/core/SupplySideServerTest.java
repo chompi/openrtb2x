029db1403b27b3c303e4e9eb63be6711b7727d01
Robert Foldes <robfoldes@gmail.com>
2011-01-14 16:39:59 -0500

    Renamed classes, added (c) and javadoc

--- supply-side/ssp-core/src/test/java/org/openrtb/ssp/core/SupplySideServerTest.java
+++ supply-side/ssp-core/src/test/java/org/openrtb/ssp/core/SupplySideServerTest.java
@@ -1,3 +1,34 @@
+/*
+ * Copyright (c) 2010, The OpenRTB Project
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   1. Redistributions of source code must retain the above copyright notice,
+ *      this list of conditions and the following disclaimer.
+ *
+ *   2. Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in the
+ *      documentation and/or other materials provided with the distribution.
+ *
+ *   3. Neither the name of the OpenRTB nor the names of its contributors
+ *      may be used to endorse or promote products derived from this
+ *      software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 package org.openrtb.ssp.core;
 
 import static org.junit.Assert.*;
@@ -18,12 +49,12 @@ import org.openrtb.common.model.AdvertiserBlocklistResponse;
 import org.openrtb.common.model.Blocklist;
 import org.openrtb.common.model.Status;
 import org.openrtb.common.util.MD5Checksum;
-import org.openrtb.ssp.OpenRtbSsp;
+import org.openrtb.ssp.SupplySideService;
 
 
-public class OpenRtbSspServerTest {
+public class SupplySideServerTest {
 
-	class OpenRtbSspTestClient implements OpenRtbSsp {
+	class OpenRtbSspTestClient implements SupplySideService {
 		@Override
 		public List<Advertiser> setBlocklists(List<Advertiser> advertisers) {
 			List<Blocklist> list1 = new LinkedList<Blocklist>();
@@ -60,14 +91,14 @@ public class OpenRtbSspServerTest {
         "  }]" +
         "}";
     
-    private OpenRtbSsp ssp;
+    private SupplySideService ssp;
     
-    private OpenRtbSspServer server;
+    private SupplySideServer server;
     
     @Before
     public void setup() {
         ssp = new OpenRtbSspTestClient();
-        server = new OpenRtbSspServer(ssp);
+        server = new SupplySideServer(ssp);
     }    
 	
     @Test
